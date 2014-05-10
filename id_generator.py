44c09798031babcda40e13ae56d4b0d22815a8d1
Herberth Amaral <herbertha.amaral@sisqualis.com.br>
2014-05-10 09:40:15 -0300

    Melhora enormemente a velocidade de execução do id_generator

--- id_generator.py
+++ id_generator.py
@@ -7,9 +7,12 @@ db = dataset.connect('postgresql+psycopg2://postgres@localhost/legendastvmirror'
 
 def run():
     table = db['shows']
+    rows = []
+    db.begin()
     for x in xrange(1, NUM_SHOWS):
         reg = dict(show_id=x, exists=None, last_change_time=datetime.datetime.min, show_name='', status='')
         table.insert(reg, ensure=False)
+    db.commit()
 
 if __name__ == '__main__':
     run()
