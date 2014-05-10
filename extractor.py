c05aa5eb4b4524a1a58fa2b127ef34e17ac70d43
Mbodock (Marcus Vinicius) <mbodock@gmail.com>
2014-05-10 12:26:10 -0300

    Adicina comentários, torna código mais legível

--- extractor.py
+++ extractor.py
@@ -14,18 +14,21 @@ class Extractor(object):
 
     def work(self):
         for show in self.shows:
+            # Travando o status do show, proteção em caso de falha
             show['status'] = 'extracting'
             show['last_change_time'] = datetime.datetime.now()
             self.db['shows'].update(show, ['id'], ensure=False)
             print "Novo show."
             self.get_download_link(show)
 
+            # Atualizando o status do show, para o gordo consumir
             show['last_change_time'] = datetime.datetime.now()
             show['status'] = 'done'
             self.db['shows'].update(show, ['id'], ensure=False)
 
 
     def get_download_link(self, show):
+        # Obtendo todas as releases do show
         releases = self.db['releases'].find(id = show['id'])
         for release in releases:
             html = requests.get("http://legendas.tv" + release['release_link'])
@@ -35,11 +38,12 @@ class Extractor(object):
             download_link = raw_download_link.split("'")[1]
             print "Obtendo link para " + release['release_link']
 
+            # Altera e salva o release com o novo link para download
             release['subtitle_download_link'] = download_link
             release['last_change_time'] = datetime.datetime.now()
             release['status'] = 'extracted'
             self.db['releases'].update(release, ['id'], ensure=False)
 
-a = Extractor()
-a.work()
-
+if __name__ == "__main__":
+    a = Extractor()
+    a.work()
